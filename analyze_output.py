955e03bbe250eefc699c8449bd1044fffc260cb1
Eric Van Clepper <ericvc@uchicago.edu>
2022-08-31 14:12:48 -0500

    fixed typo in get_stokes_grad

--- analyze_output.py
+++ analyze_output.py
@@ -8,6 +8,8 @@ fargodir = 'fargoout/fargo_mid'
 partdir = 'particleout/a10_nodiff_test'
 npart = 16
 
+mesh = pt.create_mesh(fargodir,n=-1,quiet=True)
+
 def partname(n):
     return f'particle_{n}.npz'
 
@@ -28,6 +30,11 @@ def calc_stokes_numbers(r,a,rho_s=2):
 
     return a*rho_s/(rhos*cs)*Omegas
 
+def get_stopping_time(x,y,a,rho_s=2):
+    rhog = mesh.get_rho(x,y)
+    cs = mesh.get_soundspeed(x,y)
+    return a*rho_s/rhog/cs
+
 def analytic_drdt(r,a):
     sigma0 = 209 # g cm^-2       
     aspectratio = 0
@@ -47,6 +54,17 @@ def analytic_drdt(r,a):
 
     return -eta*vks/(St + 1/St)
 
+def mesh_drdt(r,a):
+    vx,vy,vz = mesh.get_gas_vel(r,0)
+    vgas = np.sqrt(vx*vx + vy*vy)
+    GM = float(mesh.variables['G'])*float(mesh.variables['MSTAR'])
+    vkep = np.sqrt(GM/r)
+    eta = 1-(vgas/vkep)**2
+    St = calc_stokes_numbers(r,a)
+
+    return -eta*vkep/(St + 1/St)
+
+
 def main():
     fig,axd = plt.subplot_mosaic(
     """
@@ -56,17 +74,29 @@ def main():
     tight_layout=True)
 
     X = np.linspace(1.5*const.AU,20*const.AU,200)
-    axd['M'].plot(X/const.AU,analytic_drdt(X,10),ls='--',c='k')
+    axd['M'].plot(X/const.AU,mesh_drdt(X,10),ls='--',c='k')
 
     for i in range(npart):
         ptout = np.load(f'{partdir}/{partname(i)}')
         times = ptout['times']
         x,y,z,vx,vy,vz = ptout['history'].T
         r = np.sqrt(x*x + y*y)
+        st0 = get_stopping_time(x[0],y[0],10)
         drdt = get_drdt(x,y,vx,vy)
-        axd['M'].plot(r/const.AU,drdt)
-        axd['r'].plot(times/const.YR,r/const.AU,c='k',alpha=0.7)
-        axd['d'].plot(times/const.YR,drdt,c='k',alpha=0.7)
+        axd['M'].plot(r/const.AU,drdt,alpha=0.3,c='b')
+        axd['r'].plot(times/const.YR,r/const.AU,c='b',alpha=0.4)
+        label = ''
+        if i == 0:
+            label = 'stopping time'
+        axd['r'].axvline(st0/const.YR,ls=':',c='grey',label=label)
+        axd['d'].plot(times/const.YR,drdt,c='b',alpha=0.4)
+        axd['d'].axvline(st0/const.YR,ls=':',c='grey',label=label)
+        print(f'stopping time = {st0/const.YR}')
+
+    axd['r'].legend()
+    axd['M'].set(xlabel='r [au]',ylabel = 'dr/dt [cm/s]')
+    axd['d'].set(xlabel='time [yrs]',ylabel='dr/dt [cm/s]',xscale='log')
+    axd['r'].set(ylabel='r [au]',xscale='log')
 
     plt.show()
 
